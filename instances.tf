f35e1ab346d2c1780897dd184e60ac0439823a0e
Osama Hassan <osama.elnaqib@vodafone.com>
2025-11-22 00:53:23 +0200

    fix

--- instances.tf
+++ instances.tf
@@ -1,11 +1,11 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-}
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   owners      = ["099720109477"] # Canonical
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+#   }
+# }
 
 resource "aws_instance" "frontend" {
   ami                    = "ami-0abcdef1234567890"
